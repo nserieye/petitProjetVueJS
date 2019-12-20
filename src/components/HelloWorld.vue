aad5611ab09901359ad73d7e6538563c7b0d4294
nserieye <nathalene.serieye@etu.univ-poitiers.fr>
2019-12-20 10:41:06 +0100

    Projet

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,40 +1,41 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    
+    <button class="outline-primary" v-on:click="recupJson">Recup JSON</button>
+    <p>{{ json }}</p>
+
   </div>
 </template>
 
 <script>
+
+const axios = require('axios');
+
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data(){
+    return{
+      json: ''
+    }
+  },
+  methods: {
+    recupJson() {
+      //axios.get('LPTLWTM.json') --> fonctionne car LPTLWTM.json dans le dossier public
+      axios.get('http://pokeapi.co/api/v2/pokemon/ditto/') //--> fonctionne
+      //axios.get('http://voca2.univ-poitiers.fr/~subrenat/toto.html') //--> Error: Network error
+      //axios.get('http://mchabaud.projet.sfasp2mi.univ-poitiers.fr/gs-jury-gestion/back-end/web/app_dev.php/etudiantsLPTLWTM') // --> Error: Network error
+      .then( (res) => {
+        //window.alert(res)
+        this.json = res;
+      })
+      .catch(function (error) {
+        window.alert(error + " " + error.response)
+      });
+    }
   }
 }
 </script>
@@ -43,16 +44,5 @@ export default {
 <style scoped>
 h3 {
   margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
 }
 </style>
